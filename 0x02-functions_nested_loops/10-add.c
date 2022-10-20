9d201eec6046035f42d3a7dc06bc73aef995843d
evansigho <evansigho4@gmail.com>
2022-10-19 20:16:04 -0700

    new

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second intger to be added.
+ *
+ * Return: The result of the ddition.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.c"
+
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ */
+intm_isaphabet(int c)
+{
+	if((c >= 'a' && c <= 'z') ||
+		(c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
