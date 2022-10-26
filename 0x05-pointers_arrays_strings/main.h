9ff1e1efb8ac9c35646c1aa4a420d85862f244a1
evansigho <evansigho4@gmail.com>
2022-10-25 22:08:02 -0700

    new commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,8 +10,8 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-void *_strcpy(char *dest, char *src);
-char _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 int _putchar(char c);
 
 #endif /*Evansigho*/
