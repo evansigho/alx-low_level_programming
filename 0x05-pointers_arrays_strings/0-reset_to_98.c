dc3f3b3e1e8493a0c3e00a90df2e80de6eaac89e
evansigho <evansigho4@gmail.com>
2022-10-25 20:47:55 -0700

    pointers array strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes the pointer and then updates the value
+ * @n: int to check
+ * Created by: 0 is success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *n);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+void *_strcpy(char *dest, char *src);
+char _atoi(char *s);
+int _putchar(char c);
+
+#endif /*Evansigho*/
