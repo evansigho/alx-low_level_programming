6f9de3f0b563f3dd83d231114edd4663aac94dec
evansigho <evansigho4@gmail.com>
2022-11-02 06:38:41 -0700

    new

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,7 @@
  */
 int check(int a, int b)
 {
-	if (b <  || b % a == 0)
+	if (b < || b % a == 0)
 		return (0);
 	else if (a > b / 2)
 		return (1);
@@ -20,8 +20,9 @@ int check(int a, int b)
 /**
  * is_prime_number - states if number is prime
  * @n:int
- * Retun:int
+ * Return:int
  */
+
 int is_prime_number(int n)
 {
 	if (n == 2)
