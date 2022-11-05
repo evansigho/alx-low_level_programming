2d11812bcb480c6e3343785ddc5ecb37cac5d196
evansigho <evansigho4@gmail.com>
2022-11-04 23:05:26 -0700

    again

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints its name, followed by a new line.
+ * main - prints its name, followed with a new line.
  * @argc: argument count
  * @argv: arguments
  *
