ebf1abe170a42e5e326e51dab5b4d37461b248b8
evansigho <evansigho4@gmail.com>
2022-11-08 15:42:58 -0800

    new commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - array for printing a string
+ * @size: number elements array
+ * @c: char
+ * Return: pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	array = (char *)malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
+}
