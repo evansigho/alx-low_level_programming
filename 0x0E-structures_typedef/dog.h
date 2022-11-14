d2e26492fcd5376553e129db36e12228565e615e
evansigho <evansigho4@gmail.com>
2022-11-14 06:21:41 -0800

    new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,6 +6,8 @@
  *
  * @name: Name of the dog
  *
+ * @age: Age of the dog
+ *
  * @owner: Owner of the dog
  */
 
@@ -24,8 +26,5 @@ typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
-
-
-
 
 #endif
