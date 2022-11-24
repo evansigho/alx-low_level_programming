d0c4bcfff25a443000c8481919aba7f037ee440a
evansigho <evansigho4@gmail.com>
2022-11-24 06:20:35 -0800

    new commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,7 +8,8 @@ void myStartupFun(void) __attribute__ ((constructor));
 
 /**
  * myStartupFun - implementation of myStartupFun
- */void myStartupFun(void)
+ */
+void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
